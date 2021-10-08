150f950d8397a34efc027f03875332ba52c6b57f
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2021-10-08 17:30:01 +0500

    09-10-2021 03:15 PM

--- script.js
+++ script.js
@@ -43,6 +43,18 @@ timer = null;
 
 }
 let para = document.querySelector('.para');
+let close = document.querySelector('#close')
 function openpara() {
-  para.style.transform="translateY(0px)";
+
+  para.style.transform="translateY(60px)";
+  para.style.opacity="1"
+  close.style.transform= 'rotate(45deg)';
+return;
+}
+function clospara() {
+  
+  para.style.transform="translateY(0)";
+  para.style.opacity="0"
+  close.style.transform= 'rotate(0deg)';
+return;
 }
