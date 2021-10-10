d23395b052904149df6e994760e813ae2dbb6739
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2021-10-10 10:33:13 +0500

    10-10-2021 10:33AM

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function openpara() {
   if (rotateIcon) {
     close.style.transform = 'rotate(45deg)';
     para.style.opacity="1";
-    para.style.transform='translateY(40px)';
+    para.style.transform='translateY(10px)';
   } else {
     close.style.transform = 'rotate(0deg)';
     para.style.opacity="0";
@@ -59,4 +59,36 @@ function openpara() {
 
   }
 }
+let paragraph = document.querySelector('.paragraph');
+let close2 = document.querySelector('#close2');
+let round = false;
+function anime(){
+  round = !round;
+  if(round){
+ paragraph.style.opacity="1";
+ close2.style.transform='rotate(45deg)';
+ paragraph.style.transform='translateY(10px)';
+}
+else{
+  paragraph.style.opacity="0";
+  close2.style.transform='rotate(0deg)';
+  paragraph.style.transform='translateY(0px)';
+}
+}
 
+let write = document.querySelector('.write');
+let lord = false;
+close3 = document.querySelector('#close3');
+function just(){
+  lord = !lord;
+  if(lord){
+close3.style.transform='rotate(45deg)';
+write.style.opacity="1";
+write.style.transform='translateY(20px)';
+}
+else {
+  close3.style.transform='rotate(0deg)';
+write.style.opacity="0";
+write.style.transform='translateY(0px)';
+}
+}
